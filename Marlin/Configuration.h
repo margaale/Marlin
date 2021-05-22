3131e812f60702fff3f793900551386f9c93bb45
Marcelo Castagna <margaale@gmail.com>
2021-05-22 11:25:42 -0300

    add missing pins for runout sensor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1411,6 +1411,9 @@
   #define FIL_RUNOUT_ENABLED_DEFAULT true // Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   2          // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
 
+  #define FIL_RUNOUT_PIN 57
+  #define FIL_RUNOUT2_PIN 58
+
   #define FIL_RUNOUT_STATE     HIGH       // Pin state indicating that filament is NOT present.
   #define FIL_RUNOUT_PULLUP               // Use internal pullup for filament runout pins.
   //#define FIL_RUNOUT_PULLDOWN           // Use internal pulldown for filament runout pins.
